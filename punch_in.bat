acc82e4ae551d907aae547d24f3c11f637753086
Connor <50525187+WobblyRonnoc@users.noreply.github.com>
2023-07-14 12:40:01 -0400

    added simple punch_in/out batch files

---
+++ punch_in.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set "python_script=clock.py"
+
+echo Punching in...
+python "%python_script%" in
+
+pause
---
+++ punch_out.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set "python_script=clock.py"
+
+echo Punching out...
+python "%python_script%" out
+
+pause
